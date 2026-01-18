6ef6c7e9856f2732de037c05fe35f22c6461f733
luna111122 <hyewonkwon100@gmail.com>
2026-01-18 15:37:05 +0900

    feat: 토스 빌링키 요청 구현

---
+++ src/main/java/org/muses/backendbulidtest251228/domain/toss/TossBillingClient.java
@@ -0,0 +1,43 @@
+package org.muses.backendbulidtest251228.domain.toss;
+
+import lombok.extern.slf4j.Slf4j;
+import org.muses.backendbulidtest251228.domain.toss.dto.BillingIssueReqDTO;
+import org.muses.backendbulidtest251228.domain.toss.dto.BillingIssueResDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+@Slf4j
+@Component
+public class TossBillingClient {
+
+    private final WebClient webClient;
+
+
+    public TossBillingClient(@Value("${toss.secret-key}") String secretKey) {
+        String encoded = Base64.getEncoder()
+                .encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+
+        this.webClient = WebClient.builder()
+                .baseUrl("https://api.tosspayments.com")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Basic " + encoded)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
+    }
+
+    public BillingIssueResDTO issueBillingKey(String authKey, String customerKey) {
+        BillingIssueReqDTO req = new BillingIssueReqDTO(authKey, customerKey);
+
+        return webClient.post()
+                .uri("/v1/billing/authorizations/issue")
+                .bodyValue(req)
+                .retrieve()
+                .bodyToMono(BillingIssueResDTO.class)
+                .block();
+    }
+}
