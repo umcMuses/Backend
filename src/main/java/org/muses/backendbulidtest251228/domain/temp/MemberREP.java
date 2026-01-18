7e06e0617666626a03689036a7e23bd8dcbbb93a
luna111122 <hyewonkwon100@gmail.com>
2026-01-18 14:28:15 +0900

    chore: 임시 엔티티 생성

---
+++ src/main/java/org/muses/backendbulidtest251228/domain/temp/MemberREP.java
@@ -0,0 +1,7 @@
+package org.muses.backendbulidtest251228.domain.temp;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberREP extends JpaRepository<Member, Long> {
+}
---
+++ src/main/java/org/muses/backendbulidtest251228/domain/temp/ProjectREP.java
@@ -0,0 +1,6 @@
+package org.muses.backendbulidtest251228.domain.temp;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectREP extends JpaRepository<Project, Long> {
+}
