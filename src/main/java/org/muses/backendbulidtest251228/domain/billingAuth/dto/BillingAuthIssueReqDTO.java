2f5c87aad4be641477ac16d604b793379808fb35
luna111122 <hyewonkwon100@gmail.com>
2026-01-19 22:52:45 +0900

    feat: 토스페이 호출 구현

--- src/main/java/org/muses/backendbulidtest251228/domain/billingAuth/dto/BillingAuthIssueReqDTO.java
+++ src/main/java/org/muses/backendbulidtest251228/domain/billingAuth/dto/BillingAuthIssueReqDTO.java
@@ -1,7 +1,6 @@
 package org.muses.backendbulidtest251228.domain.billingAuth.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
--- src/main/java/org/muses/backendbulidtest251228/domain/toss/TossBillingClient.java
+++ src/main/java/org/muses/backendbulidtest251228/domain/toss/TossBillingClient.java
@@ -1,6 +1,9 @@
 package org.muses.backendbulidtest251228.domain.toss;
 
 import lombok.extern.slf4j.Slf4j;
+import org.muses.backendbulidtest251228.domain.billingAuth.entity.BillingAuthENT;
+import org.muses.backendbulidtest251228.domain.toss.dto.BillingApproveReqDTO;
+import org.muses.backendbulidtest251228.domain.toss.dto.BillingApproveResDTO;
 import org.muses.backendbulidtest251228.domain.toss.dto.BillingIssueReqDTO;
 import org.muses.backendbulidtest251228.domain.toss.dto.BillingIssueResDTO;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +12,7 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -49,5 +53,31 @@ public class TossBillingClient {
                 .block();
 
         log.info("[TOSS] BillingKey revoked. billingKey={}", billingKey);
+    }
+
+    // 자동 결제 승인
+    public BillingApproveResDTO approveWithBillingKey(
+            BillingAuthENT billingAuth,
+            BigDecimal amount,
+            String title,
+            String idemKey,
+            String paymentOrderId
+    ) {
+
+
+        BillingApproveReqDTO req = BillingApproveReqDTO.builder()
+                .amount(amount)
+                .customerKey(billingAuth.getCustomerKey())
+                .orderId(paymentOrderId)
+                .orderName(title)
+                .build();
+
+        return webClient.post()
+                .uri("/v1/billing/{billingKey}", billingAuth.getBillingKey())
+                .header("Idempotency-Key", idemKey)
+                .bodyValue(req)
+                .retrieve()
+                .bodyToMono(BillingApproveResDTO.class)
+                .block();
     }
 }
