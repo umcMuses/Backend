e2cf4e5015ed9e1da5d98b2012ef26a0a58590fb
luna111122 <hyewonkwon100@gmail.com>
2026-01-20 19:03:22 +0900

    fix: 결제 메서드 이름 변경, 추가

--- src/main/java/org/muses/backendbulidtest251228/domain/payment/repository/PaymentREP.java
+++ src/main/java/org/muses/backendbulidtest251228/domain/payment/repository/PaymentREP.java
@@ -3,11 +3,14 @@ package org.muses.backendbulidtest251228.domain.payment.repository;
 import org.muses.backendbulidtest251228.domain.payment.entity.PaymentENT;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PaymentREP extends JpaRepository<PaymentENT, Long> {
 
 
-    Optional<PaymentENT> findByOrderId(Long orderId);
+    Optional<PaymentENT> findByOrder_Id(Long orderId);
+
+    List<PaymentENT> findAllByOrder_Id(Long orderId);
 
 }
