c096e1e6ea4662d4bb2e7c358f3b77538050f0e5
luna111122 <hyewonkwon100@gmail.com>
2026-01-21 14:34:57 +0900

    chore: 스케줄러 설정 파일 위치 이동

--- src/main/java/org/muses/backendbulidtest251228/global/config/scheduler/SchedulingConfig.java
+++ src/main/java/org/muses/backendbulidtest251228/global/config/scheduler/SchedulingConfig.java
@@ -1,4 +1,4 @@
-package org.muses.backendbulidtest251228.global.config;
+package org.muses.backendbulidtest251228.global.config.scheduler;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
