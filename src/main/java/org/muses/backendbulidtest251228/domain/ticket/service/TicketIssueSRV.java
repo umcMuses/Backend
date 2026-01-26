ed0e07f4f2e63f93f69b60bc53357b77b060143e
luna111122 <hyewonkwon100@gmail.com>
2026-01-27 00:23:09 +0900

    feat: 티켓 생성

---
+++ src/main/java/org/muses/backendbulidtest251228/domain/ticket/service/TicketIssueSRV.java
@@ -0,0 +1,33 @@
+package org.muses.backendbulidtest251228.domain.ticket.service;
+
+import lombok.RequiredArgsConstructor;
+import org.muses.backendbulidtest251228.domain.checkin.generator.TokenGenerator;
+import org.muses.backendbulidtest251228.domain.orderItem.entity.OrderItemENT;
+import org.muses.backendbulidtest251228.domain.ticket.entity.TicketENT;
+
+import org.muses.backendbulidtest251228.domain.ticket.enums.TicketStatus;
+import org.muses.backendbulidtest251228.domain.ticket.repository.TicketRepo;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TicketIssueSRV {
+
+    private final TicketRepo ticketRepo;
+    private final TokenGenerator tokenGenerator;
+
+    // 티켓 생성 OrderItem 1개당 티켓 하나 생성
+    @Transactional
+    public void issueIfAbsent(OrderItemENT orderItem) {
+
+        TicketENT ticket = TicketENT.builder()
+                .orderItem(orderItem)
+                .ticketToken(tokenGenerator.generate())
+                .status(TicketStatus.UNUSED)
+                .build();
+
+        ticketRepo.save(ticket);
+    }
+}
